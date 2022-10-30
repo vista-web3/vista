f66c30a1d6ee00bb2892f02823b4cdd1217d92c4
Kanwar <24398939+KNWR@users.noreply.github.com>
2022-10-30 04:30:18 +0000

    routes and gitignore

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resource :communities, only: [:create, :destroy, :show]
+  resource :users_communities_memberships, only: [:create, :destroy, :update, :show]
 end
